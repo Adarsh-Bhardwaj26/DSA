2971389f4bc15b61fd13ec6159a55ff1e5bd5716
Adarsh Bhardwaj <adarshbhardwajlnc@gmail.com>
2026-07-25 21:57:28 +0530

    added brute force approach

--- String/Q647.cpp
+++ String/Q647.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 using namespace std;
+/**
+ * optimised approach
+ */
 int expand (int a,int b,string s){
         int ans=0;
         while(a>=0 && b<s.size() && s[a]==s[b]){
@@ -17,9 +20,41 @@ int expand (int a,int b,string s){
         }
         return ans;
     }
+/*
+brute force approach
+
+*/ 
+ int countSubstringsBruteForce(string s) {
+        int n = s.size();
+        int count = 0;
+
+        for (int l = 0; l < n; l++) {
+            for (int r = l; r < n; r++) {
+
+                bool palindrome = true;
+                int i = l;
+                int j = r;
+
+                while (i < j) {
+                    if (s[i] != s[j]) {
+                        palindrome = false;
+                        break;
+                    }
+                    i++;
+                    j--;
+                }
+
+                if (palindrome)
+                    count++;
+            }
+        }
+
+        return count;
+    }
     int main(){
         cout<<"Enter the string: ";
         string s;
         cin>>s;
         cout<<countSubstrings(s)<<endl;
+        cout<<countSubstringsBruteForce(s)<<endl;
     }
